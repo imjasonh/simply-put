16e5cf4fa15fad10e335449f8877dd665d621277
Jason Hall <imjasonh@gmail.com>
2012-12-11 10:53:21 -0500

    Add kind/ID paths, bucketed by user ID

--- datastore-api/datastoreapi.go
+++ datastore-api/datastoreapi.go
@@ -3,23 +3,27 @@ package datastoreapi
 // TODO: Support other request/response formats besides JSON (e.g., xml, gob)
 // TODO: Figure out if PropertyList can support nested objects, or fail if they are detected.
 // TODO: Add rudimentary single-property queries, pagination, sorting, etc.
-// TODO: Allow clients to specify the Kind? Namespace datastore by user identity (and maintain user identity)?
 
 import (
 	"appengine"
 	"appengine/datastore"
 	"appengine/urlfetch"
 	"encoding/json"
+	"errors"
+	"fmt"
 	"io"
 	"net/http"
+	"regexp"
 	"strconv"
+	"strings"
 	"time"
 )
 
 const (
-	kind         = "JsonObject"
 	idKey        = "_id"
 	createdKey   = "_created"
+	kindKey      = "_kind"
+	updatedKey   = "_updated"
 	defaultLimit = 10
 )
 
@@ -37,6 +41,7 @@ type UserInfo struct {
 	Id string
 }
 
+// getUserId gets the Google User ID for an access token.
 func getUserId(accessToken string, client http.Client) (id string, err error) {
 	resp, err := client.Get("https://www.googleapis.com/oauth2/v1/userinfo?access_token=" + accessToken)
 	if err != nil {
@@ -46,21 +51,34 @@ func getUserId(accessToken string, client http.Client) (id string, err error) {
 	if err = json.NewDecoder(resp.Body).Decode(&info); err != nil {
 		return
 	}
-    id = info.Id
-    return
+	id = info.Id
+	if id == "" {
+		err = errors.New("Invalid auth")
+	}
+	return
 }
 
-func getKind(path string) string {
-	return path[len("/datastore/v1dev/objects/"):]
-}
-
+// getKindAndId parses the kind and ID from a request path.
 func getKindAndId(path string) (kind string, id int64, err error) {
-    kind = path[len("/datastore/v1dev/objects/"):strings.LastIndex(path, "/")]
-	idStr := path[strings.LastIndex(path, "/")+1:]
-	id, err = strconv.ParseInt(idStr, 10, 64)
-	if err != nil {
+	var match bool
+	if match, err = regexp.MatchString("/datastore/v1dev/objects/[a-zA-Z]+/[0-9]+", path); err != nil {
+		return
+	} else if match {
+		kind = path[len("/datastore/v1dev/objects/"):strings.LastIndex(path, "/")]
+		idStr := path[strings.LastIndex(path, "/")+1:]
+		id, err = strconv.ParseInt(idStr, 10, 64)
+		if err != nil {
+			return
+		}
 		return
 	}
+	if match, err = regexp.MatchString("/datastore/v1dev/objects/[a-zA-Z]+", path); err != nil {
+		return
+	} else if match {
+		kind = path[len("/datastore/v1dev/objects/"):]
+		return
+	}
+	err = errors.New("Invalid path")
 	return
 }
 
@@ -78,51 +96,48 @@ func datastoreApi(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Unauthorized", http.StatusUnauthorized)
 		return
 	}
-	_ /*userId*/, err := getUserId(accessToken, *client)
+	userId, err := getUserId(accessToken, *client)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
-	if r.URL.Path == "/datastore/v1dev/objects" {
+	kind, id, err := getKindAndId(r.URL.Path)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	dsKind := fmt.Sprintf("%s--%s", userId, kind)
+
+	if id == 0 {
 		switch r.Method {
 		case "POST":
-			insert(w, r.Body, c)
+			insert(w, dsKind, r.Body, c)
 			return
 		case "GET":
 			// TODO: Parse user request into UserQuery and pass to list method
-			list(w, UserQuery{}, c)
+			list(w, dsKind, UserQuery{}, c)
 			return
 		}
 	} else {
-		sid := r.URL.Path[len("/datastore/v1dev/objects/"):]
-		if sid == "" {
-			http.Error(w, "Must specify ID", http.StatusBadRequest)
-			return
-		}
-		id, err := strconv.ParseInt(sid, 10, 64)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest)
-			return
-		}
-
 		switch r.Method {
 		case "GET":
-			get(w, id, c)
+			get(w, dsKind, id, c)
 			return
 		case "DELETE":
-			delete(w, id, c)
+			delete(w, dsKind, id, c)
 			return
 		case "POST":
 			// This is strictly "replace all properties/values", not "add new properties, update existing"
-			update(w, id, r.Body, c)
+			update(w, dsKind, id, r.Body, c)
 			return
 		}
 	}
 	http.Error(w, "Unsupported Method", http.StatusMethodNotAllowed)
 }
 
-func delete(w http.ResponseWriter, id int64, c appengine.Context) {
+func delete(w http.ResponseWriter, kind string, id int64, c appengine.Context) {
 	k := datastore.NewKey(c, kind, "", id, nil)
 	if err := datastore.Delete(c, k); err != nil {
 		if err == datastore.ErrNoSuchEntity {
@@ -134,7 +149,7 @@ func delete(w http.ResponseWriter, id int64, c appengine.Context) {
 	}
 }
 
-func get(w http.ResponseWriter, id int64, c appengine.Context) {
+func get(w http.ResponseWriter, kind string, id int64, c appengine.Context) {
 	k := datastore.NewKey(c, kind, "", id, nil)
 	var plist datastore.PropertyList
 	if err := datastore.Get(c, k, &plist); err != nil {
@@ -146,10 +161,12 @@ func get(w http.ResponseWriter, id int64, c appengine.Context) {
 		return
 	}
 	m := plistToMap(plist, k)
+	m[idKey] = k.IntID()
+	m[kindKey] = kind
 	json.NewEncoder(w).Encode(m)
 }
 
-func insert(w http.ResponseWriter, r io.Reader, c appengine.Context) {
+func insert(w http.ResponseWriter, kind string, r io.Reader, c appengine.Context) {
 	var m map[string]interface{}
 	if err := json.NewDecoder(r).Decode(&m); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -166,6 +183,7 @@ func insert(w http.ResponseWriter, r io.Reader, c appengine.Context) {
 		return
 	}
 	m[idKey] = k.IntID()
+	m[kindKey] = kind
 	json.NewEncoder(w).Encode(m)
 }
 
@@ -209,7 +227,7 @@ func mapToPlist(m map[string]interface{}) datastore.PropertyList {
 	return plist
 }
 
-func list(w http.ResponseWriter, uq UserQuery, c appengine.Context) {
+func list(w http.ResponseWriter, kind string, uq UserQuery, c appengine.Context) {
 	limit := 3
 	q := datastore.NewQuery(kind).Limit(limit)
 
@@ -227,6 +245,7 @@ func list(w http.ResponseWriter, uq UserQuery, c appengine.Context) {
 			return
 		}
 		m := plistToMap(plist, k)
+		m[kindKey] = kind
 		items = append(items, m)
 		if crs, err = t.Cursor(); err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -240,12 +259,13 @@ func list(w http.ResponseWriter, uq UserQuery, c appengine.Context) {
 	json.NewEncoder(w).Encode(r)
 }
 
-func update(w http.ResponseWriter, id int64, r io.Reader, c appengine.Context) {
+func update(w http.ResponseWriter, kind string, id int64, r io.Reader, c appengine.Context) {
 	var m map[string]interface{}
 	if err := json.NewDecoder(r).Decode(&m); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
+	m[updatedKey] = time.Now()
 
 	plist := mapToPlist(m)
 
@@ -255,5 +275,6 @@ func update(w http.ResponseWriter, id int64, r io.Reader, c appengine.Context) {
 		return
 	}
 	m[idKey] = id
+	m[kindKey] = kind
 	json.NewEncoder(w).Encode(m)
 }
